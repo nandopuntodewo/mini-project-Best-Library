bdfd44e829df39d80405e5c99449c7dbd1b65c60
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2023-10-30 10:28:40 +0700

    Update chatbot.jsx

--- best-library/src/pages/chatbot.jsx
+++ best-library/src/pages/chatbot.jsx
@@ -6,7 +6,6 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
 });
 
 export default function Chatbot() {
